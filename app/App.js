eab1b9279c537e59750f727af0df4d055fa1d0d1
Amillz70 <adammiller7@yahoo.com>
2018-12-04 14:03:30 -0500

    Add index route in server.js
    Refactored App.js to attempt to connect with front end
    More work needs to be done

--- app/App.js
+++ app/App.js
@@ -1,29 +1,53 @@
 const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
-const axios = require('axios')
+// const http = require('http')
+// const socketIo = require('socket.io')
+// const axios = require('axios')
 
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4741
 const index = require('./routes/index')
 
 const app = express()
 app.use(index)
-
-const server = http.createServer(app)
-const io = socketIo(server)
-
-io.on('connection', socket => {
-  // console.log('New client connected'), setInterval(
-  //   () => getApiAndEmit(socket),
-  //   10000
-  // )
-  socket.on('disconnect', () => console.log('Client disconnected'))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4741')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,Authorization')
+  next()
 })
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+// let interval
+// io.on('connection', socket => {
+//   io.listen(port)
+//   console.log('listen on port ', port)
+//   if (interval) {
+//     clearInterval(interval)
+//   }
+//   interval = setInterval(() => getApiAndEmit(socket), 10000)
+//   socket.on('disconnect', () => {
+//     console.log('Client disconnected')
+//   })
+// })
+
+require('socket.io')(server, {origins: 'domain.com:* localhost:7165:* localhost:7165:*'})
+
+// io.origins(['localhost:4741'])
+//
+// io.on('connection', (client) => {
+//   // here you can start emitting events to the client
+//   console.log('connected')
+// })
+
+server.listen(port)
 
 // const getApiAndEmit = async socket => {
 //   try {
 //     const res = await axios.get(
-//       'https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558'
+//       ''
 //     )
 //     socket.emit('FromAPI', res.data.currently.temperature)
 //   } catch (error) {
@@ -31,4 +55,4 @@ io.on('connection', socket => {
 //   }
 // }
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+// server.listen(port, () => console.log(`Listening on port ${port}`))
